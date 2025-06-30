882ff5b83b28d8546b61e2cc9bfb352932544ce2
raphamendes00 <mendesraphael4@gmail.com>
2025-06-30 20:31:55 -0300

    Inicial commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+      <div id="app">
+        <header>
+          <v-app-bar :elevation="22">
+            <template>
+              <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
+          </v-app-bar>
+        </header>
+        <main>
+          <router-view></router-view>
+        </main>
+        <footer>
+          <p>Desenvolvido por Raphael Mendes</p>
+        </footer>
+      </div>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+import index from './pages/index.vue';
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto p-4">
+        <template>
+            <h1 class="text-3x1 font-bold">Bem vindo à Max Lumer Luminárias</h1>
+        </template>
+        <p class="my-4">Explore nossos produtos e saiba mais sobre nós!</p>
+        <div class="space-x-4">
+            
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
